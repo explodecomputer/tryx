b8c5e29a48ea6ea1570a5b11bed3fc9231d19102
Gibran Hemani <explodecomputer@gmail.com>
2018-06-17 09:45:55 +0100

    finished mv implementation

--- tests/ldsr.r
+++ tests/ldsr.r
@@ -28,18 +28,19 @@ id_remove <- unique(c(
 	as.character(subset(chd, abs(rg) > 0.5)$id.y)
 ))
 
+library(tryx)
 ex <- extract_instruments("UKB-a:360")
 ou <- extract_outcome_data(ex$SNP, 7)
 d <- harmonise_data(ex, ou)
-tryxscan <- tryx.scan(d, use_proxies=TRUE)
+tryxscan <- tryx.scan(d)
 tryxscan <- tryx.sig(tryxscan)
 
+# Looking at the significant candidate traits, remove the following because they are too similar to SBP or CHD
 id_remove <- c("UKB-a:61","UKB-a:116","UKB-a:117","UKB-a:121","UKB-a:131","UKB-a:132","UKB-a:148","UKB-a:149","UKB-a:150","UKB-a:161","UKB-a:162","UKB-a:164","UKB-a:168","UKB-a:177","UKB-a:193","UKB-a:202","UKB-a:207","UKB-a:211","UKB-a:215","UKB-a:218","UKB-a:222","UKB-a:359","UKB-a:360","UKB-a:392","UKB-a:435","UKB-a:437","UKB-a:449","UKB-a:450","UKB-a:452","UKB-a:489","UKB-a:490","UKB-a:531","472","733","UKB-a:41","UKB-a:62","UKB-a:63","UKB-a:108","UKB-a:118","UKB-a:165","UKB-a:176","UKB-a:180","UKB-a:191","UKB-a:201","UKB-a:209","UKB-a:217","UKB-a:345","UKB-a:434","UKB-a:436","UKB-a:448","UKB-a:488","6","7","8","9","113","122","229","UKB-a:532","UKB-a:533","UKB-a:534","298","UKB-a:568","760","766","1108","1109","1110","798","UKB-a:581","1092","1094","UKB-a:24","UKB-a:310","UKB-a:22")
 
 ta <- tryx.analyse.mv(tryxscan, id_remove=id_remove)
 ta$plot
-
-
+ta$estimate
 
 
 
